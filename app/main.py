c3338be5e7ddfe99ec17ce005653b1813cb53a78
Tim Langford <tim.langford@gmail.com>
2024-06-15 22:16:42 +0100

    Added a simple HTTP 200 response.

--- app/main.py
+++ app/main.py
@@ -2,13 +2,26 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    print(f'Starting Server...')
 
-    # Uncomment this to pass the first stage
-    #
+    # Open a TCP socket on localhost:4221 and wait for client connections
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print(f'Server is listening on {server_socket.getsockname()}...')
+
+    while True:
+        # 1. Accept the client connection.
+        client_socket, client_address = server_socket.accept()
+        print(f'Accepted connection from {client_address}')
+
+        # 2. Prepare the client response.
+        response = b'HTTP/1.1 200 OK\r\n\r\n'
+
+        # 3. Send the response to the client.
+        client_socket.sendall(response)
+
+        # 4. Gracefully shutdown and close the connection.
+        client_socket.shutdown(socket.SHUT_RDWR)
+        client_socket.close()
 
 
 if __name__ == "__main__":
---
+++ notes.md
@@ -0,0 +1,13 @@
+# Notes
+
+### Connect to Server using Netcat
+
+```sh
+nc -vz 127.0.0.1 4221
+```
+
+### HTTP Request
+
+```sh
+curl -v http://localhost:4221
+```
