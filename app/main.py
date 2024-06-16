3861c0fcb132edfb0eb5f8631b4cce410010c43d
Tim Langford <tim.langford@gmail.com>
2024-06-16 11:14:05 +0100

    Added verb, path, protocol, headers, body parsing and extraction. Returns a 200 for route paths, and 404 for no routes paths.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import socket
 
-
 def main():
     print(f'Starting Server...')
 
@@ -9,17 +8,49 @@ def main():
     print(f'Server is listening on {server_socket.getsockname()}...')
 
     while True:
-        # 1. Accept the client connection.
+        # Accept the client connection.
         client_socket, client_address = server_socket.accept()
         print(f'Accepted connection from {client_address}')
 
-        # 2. Prepare the client response.
-        response = b'HTTP/1.1 200 OK\r\n\r\n'
+        # Receive the request data from the client.
+        request_data = client_socket.recv(1024).decode('utf-8')
 
-        # 3. Send the response to the client.
+        # Split request data into lines
+        LINE_DELIMITER = "\r\n"
+        request_lines = request_data.split(LINE_DELIMITER)
+        
+        # Extract and print the request line
+        request_line = request_lines[0]
+        print(f"Request Line: {request_line}")
+        verb, path, protocol = request_line.split(' ')
+        print(f'Verb: {verb}., path: {path}, protocol: {protocol}')
+
+        # Initialize headers dictionary
+        headers = {}
+        current_line = 1
+        # Parse Headers
+        for line in request_lines[1:]:
+            if not line:
+                break
+            key, value = line.split(':', 1)
+            headers[key.strip()] = value.strip()
+            print(f'Header: {key} = {value}')
+            current_line += 1
+
+        # Extract and print the request body (if present)
+        request_body = request_lines[current_line+1:]
+        print(f"Request Body: {request_body}")
+
+        # Prepare the client response.
+        if path == '/':
+            response = b'HTTP/1.1 200 OK\r\n\r\n'
+        else:
+            response = b'HTTP/1.1 404 Not Found\r\n\r\n'
+
+        # Send the response to the client.
         client_socket.sendall(response)
 
-        # 4. Gracefully shutdown and close the connection.
+        # Gracefully shutdown and close the connection.
         client_socket.shutdown(socket.SHUT_RDWR)
         client_socket.close()
 
