0529fa79bdaddd1680c70cd58575177477887413
Tim Langford <tim.langford@gmail.com>
2024-06-21 21:00:17 +0100

    Added gzip body support.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,33 @@
 import argparse
+import base64
+from concurrent.futures import ThreadPoolExecutor
+import gzip
+import io
 import socket
 import threading
-from concurrent.futures import ThreadPoolExecutor
+import traceback
 
+# Manual Curl Tests -------------------------------------------------------------------------------
+# 
+
+# curl -v http://localhost:4221 ;echo
+# curl -v http://localhost:4221/unknown ;echo
+# curl -v http://localhost:4221/echo/Hello ;echo
+# curl -v http://localhost:4221/echo -d 'Hello' ;echo
+# curl -v --header "User-Agent: foobar/1.2.3" http://localhost:4221/user-agent ;echo
+# curl -i -X POST http://localhost:4221/files/foo -d 'Hello, World!' ;echo
+# curl -i http://localhost:4221/files/foo ;echo
+# curl -i http://localhost:4221/files/non_existant_file :echo
+# curl -v --data "12345" -H "Content-Type: application/octet-stream" http://localhost:4221/files/file_123 :echo
+# curl -v -H "Accept-Encoding: gzip" http://localhost:4221/echo/abc | hexdump -C
+
+
+# Classes -----------------------------------------------------------------------------------------
+# 
 
 CLRF = "\r\n"
 
-class HTTPRequest:
+class HttpRequest:
     def __init__(self, request_data):
         request_lines = request_data.split(CLRF)
         current_line = 1
@@ -47,11 +68,11 @@ class HTTPRequest:
     def __str__(self):
         return f"Request Line: {self.request_line}, Headers: {self.headers}, Request Body: {self.request_body}"
 
-class HTTPResponse:
-    def __init__(self, rq: HTTPRequest):
-        self.rq = rq
+class HttpResponse:
+    def __init__(self, http_request: HttpRequest):
+        self.http_request = http_request
         
-        self.protocol = rq.get_protocol()
+        self.protocol = http_request.get_protocol()
         self.status_code = 0
         self.reason = None
 
@@ -60,30 +81,38 @@ class HTTPResponse:
 
         self.body = None
         self.encoding = 'utf-8'
+        self.compressed = False
     
-    def with_status(self, status_code, reason) -> 'HTTPResponse':
+    def with_status(self, status_code, reason) -> 'HttpResponse':
         self.status_code = status_code
         self.reason = reason
         return self
 
-    def with_body(self, body: str, encoding="utf-8", content_type: str=None) -> 'HTTPResponse':
+    def with_body(self, body, content_type: str=None) -> 'HttpResponse':
         self.body = body
-        self.encoding = encoding
         self.headers['Content-Length'] = len(body)
         if content_type:
             self.headers['Content-Type'] = content_type
         return self
 
-    def as_http_response(self) -> str:
-        response = f'{self.protocol} {self.status_code} {self.reason}' + CLRF
-        for key, value in self.headers.items():
-            response += f'{key}: {value}' + CLRF
-        response += CLRF
-        response += self.body if self.body else ''
-        return response
-
     def as_http_response_bytes(self) -> bytes:
-        return self.as_http_response().encode(self.encoding)
+        body_bytes: bytes = b''
+        if self.body:
+            if self.encoding == 'gzip':
+                self.with_body(gzip_string(self.body), content_type='text/plain')
+                self.headers['Content-Encoding'] = 'gzip'
+                body_bytes = self.body
+            else :
+                body_bytes = self.body.encode(self.encoding)
+
+        headers_bytes: bytes = b''
+        for key, value in self.headers.items():
+            headers_bytes += (f'{key}: {value}' + CLRF).encode('utf-8')
+        headers_bytes += CLRF.encode('utf-8')
+
+        status_bytes: bytes = (f'{self.protocol} {self.status_code} {self.reason}' + CLRF).encode('utf-8')
+
+        return status_bytes + headers_bytes + body_bytes
 
 class HttpContext:
     def __init__(self):
@@ -93,32 +122,63 @@ class HttpContext:
         self.directory = directory
         return self
 
-def handle_request(context: HttpContext, request: HTTPRequest) -> HTTPResponse:
+
+# Support Functions -------------------------------------------------------------------------------
+# 
+
+def encode_bytes_to_base64(byte_data):
+    # Encode bytes to Base64 string
+    base64_str = base64.b64encode(byte_data).decode('utf-8')
+    return base64_str
+
+
+def decode_base64_to_bytes(base64_str):
+    # Decode Base64 string back to bytes
+    byte_data = base64.b64decode(base64_str.encode('utf-8'))
+    return byte_data
+
+
+def gzip_string(input_string) -> bytes:
+    # Create a BytesIO buffer to hold the compressed data
+    mem_buf = io.BytesIO()
+    
+    # Use the gzip module to write the compressed data to the buffer
+    with gzip.GzipFile(fileobj=mem_buf, mode='wb') as f:
+        f.write(input_string.encode('utf-8'))
+    
+    # Get the compressed data from the buffer
+    return mem_buf.getvalue()
+
+
+# HTTP WebServer Functions ------------------------------------------------------------------------
+# 
+
+def handle_request(context: HttpContext, request: HttpRequest) -> HttpResponse:
     path = request.get_path()
-    response : HTTPResponse = None
+    response : HttpResponse = None
     # Prepare the client response.
     if path == '/':
         # curl -v http://localhost:4221; echo
-        response = HTTPResponse(request).with_status(200, 'OK')
+        response = HttpResponse(request).with_status(200, 'OK')
     elif path.startswith('/echo'):
         echo_path_args = path.replace('/echo', '', 1)
         if (len(echo_path_args) > 1):
             # curl -v http://localhost:4221/echo/Hello; echo
             rs_body = echo_path_args[1:]
-            response = HTTPResponse(request).with_status(200, 'OK')\
-                .with_body(rs_body, encoding="utf-8", content_type='text/plain')
+            response = HttpResponse(request).with_status(200, 'OK')\
+                .with_body(rs_body, content_type='text/plain')
         elif (len(request.get_body()) > 1):
             # curl -v http://localhost:4221/echo -d 'Hello'; echo
             rs_body = request.get_body() 
-            response = HTTPResponse(request).with_status(200, 'OK')\
-                .with_body(rs_body, encoding="utf-8", content_type='text/plain')
+            response = HttpResponse(request).with_status(200, 'OK')\
+                .with_body(rs_body, content_type='text/plain')
         else:
-            response = HTTPResponse(request).with_status(200, 'OK')
+            response = HttpResponse(request).with_status(200, 'OK')
     elif path.startswith('/user-agent'):
-        # c
+        # curl -v --header "User-Agent: foobar/1.2.3" http://localhost:4221/user-agent; echo
         rs_body = request.get_header('User-Agent')
-        response =  HTTPResponse(request).with_status(200, 'OK')\
-            .with_body(rs_body, encoding="utf-8", content_type='text/plain')
+        response =  HttpResponse(request).with_status(200, 'OK')\
+            .with_body(rs_body, content_type='text/plain')
     elif request.get_verb() == 'GET' and path.startswith('/files'):
         print(f'GET request for file: {path}')
         path_parts = path.split('/')
@@ -129,18 +189,17 @@ def handle_request(context: HttpContext, request: HTTPRequest) -> HTTPResponse:
                 # curl -i http://localhost:4221/files/foo
                 with open(f'{context.directory}/{file_path}', 'r') as file:
                     body = file.read()
-                    response = HTTPResponse(request).with_status(200, 'OK')\
+                    response = HttpResponse(request).with_status(200, 'OK')\
                         .with_body(body, encoding='utf-8', content_type='application/octet-stream')
             except FileNotFoundError:
                 # curl -i http://localhost:4221/files/non_existant_file
-                response = HTTPResponse(request).with_status(404, 'Not Found')
+                response = HttpResponse(request).with_status(404, 'Not Found')
             except Exception as e:
                 print(f"Error reading file: {e}")
-                response = HTTPResponse(request).with_status(500, 'Internal Server Error')
+                response = HttpResponse(request).with_status(500, 'Internal Server Error')
         else:
-            response = HTTPResponse(request).with_status(404, 'Not Found')
+            response = HttpResponse(request).with_status(404, 'Not Found')
     elif request.get_verb() == 'POST' and path.startswith('/files'):
-        print(f'POST request for file: {path}')
         path_parts = path.split('/')
         if len(path_parts) == 3:
             file_path = path_parts[2]
@@ -149,39 +208,46 @@ def handle_request(context: HttpContext, request: HTTPRequest) -> HTTPResponse:
                 # curl -v --data "12345" -H "Content-Type: application/octet-stream" http://localhost:4221/files/file_123
                 with open(f'{context.directory}/{file_path}', 'w') as file:
                     file.write(request.get_body())
-                    response = HTTPResponse(request).with_status(201, 'Created')
+                    response = HttpResponse(request).with_status(201, 'Created')
             except Exception as e:
                 print(f"Error writing file: {e}")
-                response = HTTPResponse(request).with_status(500, 'Internal Server Error')
+                response = HttpResponse(request).with_status(500, 'Internal Server Error')
         pass
     else:
         # curl -v http://localhost:4221/unknown; echo
-        response = HTTPResponse(request).with_status(404, 'Not Found')
+        response = HttpResponse(request).with_status(404, 'Not Found')
 
     return response
+
+
+def handle_response(context: HttpContext, response: HttpResponse) -> HttpResponse:
+    encodings = response.http_request.get_header('Accept-Encoding')
+    encoding_list = encodings.split(',') if encodings else []
+    for encoding in encoding_list:
+        if encoding and encoding.strip().lower() == 'gzip':
+            # curl -v -H "Accept-Encoding: gzip" http://localhost:4221/echo/abc | hexdump -C
+            # Compress the response body using gzip encoding
+            response.encoding = encoding
+    return response
+
 
 def handle_client(context: HttpContext, client_socket, client_address):
     print(f'Accepted connection from {client_address}')
     try:
-        # # Receive the request data from the client.
+        # Receive the request data from the client.
+        # TODO: Handle large requests that require multiple recv calls.
         request_data = client_socket.recv(1024).decode('utf-8')
 
-        request = HTTPRequest(request_data)
+        request = HttpRequest(request_data)
         response = handle_request(context, request)
-
-        encodings = request.get_header('Accept-Encoding')
-        encoding_list = encodings.split(',') if encodings else []
-        for encoding in encoding_list:
-            
-            if encoding and encoding.strip().lower() == 'gzip':
-                # Compress the response body using gzip encoding
-                # import gzip
-                response.headers['Content-Encoding'] = 'gzip'
+        response = handle_response(context, response)
 
         # Send the response to the client.
         client_socket.sendall(response.as_http_response_bytes())
     except Exception as e:
         print(f"Error handling client {client_address}: {e}")
+        traceback.print_exc()
+
     finally:
         # Gracefully shutdown and close the connection.
         client_socket.shutdown(socket.SHUT_RDWR)
